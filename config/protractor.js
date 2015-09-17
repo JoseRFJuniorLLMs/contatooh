5c4a63d6cbe3f6c1cf54a46cdccdfee79cd6993e
Lucas Carvalho <lucas.carvalho@bravi.com.br>
2015-09-17 16:20:59 -0300

    Chapter 12.6 - Travis

--- config/protractor.js
+++ config/protractor.js
@@ -1,6 +1,14 @@
 var config = require('./config')();
 
 exports.config = {
+  sauceUser: config.sauceUser,
+  sauceKey: cofnig.sauceKey,
+  capabilities: {
+    'name': config.sauceTestName,
+    'browserName': 'chrome',
+    'tunnel-identifier': config.travisJobNumber,
+    'build': config.travisBuild
+  },
   specs: ['../test/e2e/**/*.js'],
   onPrepare: function() {
       browser.driver.get('http://localhost:3000/');
---
+++ test/data.js
@@ -0,0 +1,32 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var contacts = [
+  {name: "xyz1", email: "xyz1@email.com"},
+  {name: "xyz2", email: "xyz2@email.com"},
+  {name: "xyz3", email: "xyz3@email.com"}
+];
+
+MongoClient.connect('mongodb://127.0.0.1:27017/contatooh_test',
+  function(err, db) {
+    if (err) {
+      throw err;
+    }
+
+    db.dropDatabase(function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('Test database has been dropped');
+
+      db.collection('contacts').insert(contacts,
+      function(error, inserted) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('Test database has been populated');
+
+        process.exit(0);
+      });
+    });
+  }
+);
